ef810107daf6e213d6e36abc1c2eb775ddcd339a
janekdererste <janek.laudan@mailbox.org>
2024-02-17 19:34:17 +0100

    Create and read records

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use crate::logging::init_logging;
-use std::sync::atomic::{AtomicU32, Ordering};
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 
 use askama::Template;
-use axum::routing::get;
-use axum::Router;
+use askama_axum::IntoResponse;
+use axum::extract::State;
+use axum::response::Redirect;
+use axum::routing::{get, post};
+use axum::{Form, Router};
+use serde::Deserialize;
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
+use tower_http::trace::TraceLayer;
 use tracing::info;
 
 mod logging;
@@ -15,20 +19,14 @@ mod logging;
 async fn main() {
     init_logging();
 
-    let reload_counter = Arc::new(AtomicU32::new(0));
-    let counter_ref = reload_counter.clone();
-
     info!("Configuring Router.");
     let router = Router::new()
-        .route(
-            "/",
-            get(move || async {
-                let counter_ref = counter_ref;
-                let count = counter_ref.fetch_add(1, Ordering::Relaxed);
-                index(count).await
-            }),
-        )
-        .nest_service("/assets", ServeDir::new("assets"));
+        .route("/", get(index))
+        .route("/create", get(create_record))
+        .route("/handle-create", post(handle_create))
+        .nest_service("/assets", ServeDir::new("assets"))
+        .layer(TraceLayer::new_for_http())
+        .with_state(AppState::default());
     info!("Setting up Listener on 0.0.0.0:3000");
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
@@ -36,13 +34,59 @@ async fn main() {
     axum::serve(listener, router).await.unwrap();
 }
 
-async fn index(i: u32) -> IndexTemplate {
-    info!("Reloaded {i} times");
-    IndexTemplate { reloads: i }
+async fn index<'a>(State(state): State<AppState>) -> IndexTemplate {
+    IndexTemplate {
+        records: state.records(),
+    }
+}
+
+async fn create_record() -> CreateRecordTemplate {
+    CreateRecordTemplate
+}
+
+async fn handle_create(
+    State(mut state): State<AppState>,
+    Form(data): Form<NewRecordForm>,
+) -> impl IntoResponse {
+    state.add_record(data);
+
+    Redirect::to("/")
 }
 
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexTemplate {
-    reloads: u32,
+    records: Vec<NewRecordForm>,
+}
+
+#[derive(Template)]
+#[template(path = "create-record.html")]
+struct CreateRecordTemplate;
+
+#[derive(Deserialize, Clone, Debug)]
+struct NewRecordForm {
+    title: String,
+    amount: f32,
+    tag: String,
+}
+
+#[derive(Debug, Default, Clone)]
+struct AppState {
+    records: Arc<RwLock<Vec<NewRecordForm>>>,
+}
+
+impl AppState {
+    fn records(&self) -> Vec<NewRecordForm> {
+        self.records
+            .read()
+            .expect("Failed to acquire lock.")
+            .clone()
+    }
+
+    fn add_record(&mut self, record: NewRecordForm) {
+        self.records
+            .write()
+            .expect("Failed to aquire lock")
+            .push(record);
+    }
 }
